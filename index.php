6d33847a69bec4538a67588e69b08cc6c5b205bb
Srdjan Grbic <srdjangrbic2004@gmail.com>
2021-01-30 21:49:08 +0100

    Beginning is done
    I manage to build database, make home page and full log in/registration mechanics.
    I am about to start making profile page.
    When I am done with that, I will maybe get back to make home a little bit better.
    Also, if you run this on your divice, open index.php, it wont work, because you do not have xampp installed and setted up, because there is no server, but if you do have xampp, then just import this database into your localhost and enjoy the show.

---
+++ index.php
@@ -0,0 +1,91 @@
+<?php
+session_start();
+ ?>
+<html>
+  <head>
+    <link rel="stylesheet" href="style.css">
+    <title>CourseFull</title>
+  </head>
+  <body>
+    <header id = "Head">
+      <h1 style = "color: #24333C; text-align: center; ">CourseFull</h1>
+    </header>
+    <main>
+      <div id = "EnterTheWebsite">
+        <h2 id = "Title">Take control of your <br> course load, set goals, and collaborate<br> to live your education to the fullest! </h2>
+        <div id = "UserRegistrationForm">
+          <form method = "POST" action = "#">
+            <h3 style = "font-size: 25px; text-align: center; ">More you learn, more you earn. </h3>
+            <input name = "RFirstName" type = "text" placeholder = "Enter your First Name"> <input name = "RLastName" type = "text" placeholder = "Enter your Last Name">
+            <input name = "REmail" type = "text" placeholder = "Enter your e-mail" class = "RegistrationInput">
+            <input name = "RPassword" type = "password" placeholder = "Enter your password" class = "RegistrationInput">
+            <button name = "UserSignIn" type = "submit" class = "ProgrammerButton">Sign in</button>
+            <?php
+              if(isset($_POST['UserSignIn'])){
+
+                $FirsName = $_POST['RFirstName'];
+                $LastName = $_POST['RLastName'];
+                $Mail = $_POST['REmail'];
+                $Password = $_POST['RPassword'];
+
+                include 'connection.php';
+                $Query = "INSERT INTO `users` VALUES (NULL, '$FirsName', '$LastName', '$Mail', '$Password')";
+                $Result = mysqli_query($Link, $Query) or die(mysqli_error());
+
+                $Number = mysqli_affected_rows($Link);
+                if($Number > 0) {
+                  echo "Registration went successful, Log in to your new account";
+                }
+                else {
+                  echo "There was an error :(, please try again, later and we are excited that you want to join CourseFull :D . )";
+                }
+
+              }
+            ?>
+        </form>
+        </div>
+        <div id = "UserLogInForm">
+          <form action = "#" method = "POST">
+            <h3 style = "font-size: 25px; text-align: center; color: #00FF66; ">Welcome Back</h3>
+            <input name = "LIMail" type = "text" placeholder = "Enter your username">
+            <input name = "LIPassword" type = "password" placeholder = "Enter your password">
+            <button name = "LogIn" type = "submit" class = "LogInButton">Log In</button>
+            <?php
+            if(isset($_POST["LogIn"])){
+              include "connection.php";
+              $Mail = $_POST['LIMail'];
+              $Password = $_POST['LIPassword'];
+              $SqlQuery = "SELECT * FROM users WHERE emailadress  = '$Mail' AND password = '$Password'";
+              $Result = mysqli_query($Link, $SqlQuery) or die ("Connection Error :(. Please try again later on or contact me on srdjangrbic@zohomail.eu");
+              $RowNumber = $Result ->num_rows;
+              if($RowNumber > 0){
+                $Row = mysqli_fetch_row($Result);
+
+                $UserId = $Row[0];
+                $FirstName = $Row[1];
+                $LastName = $Row[2];
+                $EMailAdress = $Row[3];
+                $Password = $Row[4];
+
+                $_SESSION['userid'] = $UserId;
+                $_SESSION['firstname'] = $FirstName;
+                $_SESSION['lastname'] = $LastName;
+                $_SESSION['emailadress'] = $EMailAdress;
+                $_SESSION['password'] = $Password;
+
+                header('Location: profile.php');
+              }
+              else{
+                echo "You entered wrong E-mail or Password";
+              }
+            }
+             ?>
+          </form>
+        </div>
+      </div>
+    </main>
+    <foother id = "Foother">
+      <h2 style = "color: #24333C; text-align: center; ">Created by CourseFull, all copyright reserved. </h2>
+    </foother>
+  </body>
+</html>
---
+++ users.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 30, 2021 at 09:43 PM
+-- Server version: 10.4.16-MariaDB
+-- PHP Version: 7.4.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `coursefull`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `userid` int(50) NOT NULL,
+  `firstname` varchar(50) NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `emailadress` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`userid`, `firstname`, `lastname`, `emailadress`, `password`) VALUES
+(1, 'Srdjan', 'Grbic', 'srdjangrbic10@gmail.com', 'SidjaSr04gr04UvaSidjaSr04gr04Uva'),
+(6, 'Test', 'Testic', 'Testiranje', 'Test');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`userid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `userid` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
