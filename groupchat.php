be5a94a7481ec27df41701b5c82d3c8558e30b67
Srdjan Grbic <srdjangrbic2004@gmail.com>
2021-01-30 23:36:13 +0100

    I created Group Chat Style
    I created Group Chat Style and I will start creating mechanics first.
    
    First I will make Group chat database;
    
    And then mechanics in PhP.

---
+++ groupchat.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>CourseFull - GroupChat</title>
+    <link rel = "stylesheet" href = "groupchatstyle.css">
+  </head>
+  <body>
+    <header id = "GroupHead">
+      <h1 style = "color: #24333C; text-align: center; ">CourseFull - [GroupName] - Group</h1>
+    </header>
+    <main>
+
+    </main>
+    <footer id = "GroupBottom">
+      <input name = "CInput" type = "Text" placeholder = "Share your thoughts here.....">
+      <button name = "GCSubmit">Share it!</button>
+      <a href = "profile.php"><button class = "GCProfile">Go back to profile</button></a>
+    </footer>
+  </body>
+</html>
