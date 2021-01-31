450ec7d18099e81874b37095129ec6942c91c93f
Srdjan Grbic <srdjangrbic2004@gmail.com>
2021-01-31 03:24:09 +0100

    Everything that I done so far
    I need to close my eyes for a little bit and I will see you all in 4 hours.

--- groupchat.php
+++ groupchat.php
@@ -1,20 +1,37 @@
+<?php
+  session_start();
+ ?>
 <!DOCTYPE html>
 <html>
   <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CourseFull - GroupChat</title>
     <link rel = "stylesheet" href = "groupchatstyle.css">
   </head>
   <body>
     <header id = "GroupHead">
-      <h1 style = "color: #24333C; text-align: center; ">CourseFull - [GroupName] - Group</h1>
+      <h1 style = "color: #24333C; text-align: center; ">CourseFull - <?php echo $_SESSION['groupname'] ?>  - Group</h1>
     </header>
     <main>
 
     </main>
     <footer id = "GroupBottom">
-      <input name = "CInput" type = "Text" placeholder = "Share your thoughts here.....">
-      <button name = "GCSubmit">Share it!</button>
       <a href = "profile.php"><button class = "GCProfile">Go back to profile</button></a>
+      <form method = "POST" action = "#">
+        <input name = "CInput" type = "Text" placeholder = "Share your thoughts here.....">
+        <button name = "GCSubmit">Share it!</button>
+        <?php
+          if(isset($_POST["GCSubmit"])){
+            $Messadge = $_POST['CInput'];
+            $GroupId = $_SESSION['groupid'];
+            $UserId = $_SESSION['userid'];
+            include "connection.php";
+            $MessadgeQuery = "INSERT INTO `messengers` VALUES (NULL, $GroupId, $UserId, $Messadge)";
+            $SendResult = mysqli_query($Link, $MessadgeQuery) or die ("Something went wrong :(, we could not deliver your messadge. ");
+
+          }
+         ?>
+      </form>
     </footer>
   </body>
 </html>
